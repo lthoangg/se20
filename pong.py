5c97199c2f85ebc4ee20281ab6dc6afe34d1006e
lthoangg <Hoanglt.bi9241@st.usth.edu.vn>
2020-03-04 17:21:50 +0700

    updated pong

--- pong.py
+++ pong.py
@@ -3,10 +3,11 @@ import os
 import time
 import paddle
 BALL_IMGS = [pygame.image.load(os.path.join("imgs", "ball1.png"))]
-
+bg_IMGS=[pygame.image.load(os.path.join("imgs", "bg1.png"))]
 def main():
     pygame.init()
-    WIN_WIDTH = WIN_HEIGHT = 600
+    WIN_WIDTH = 1024
+    WIN_HEIGHT = 551
     WIN = pygame.display.set_mode((WIN_WIDTH, WIN_HEIGHT))
     WIN_ICON = BALL_IMGS[0]
     pygame.display.set_icon(WIN_ICON)
@@ -16,8 +17,8 @@ def main():
     p1 =paddle.Paddle()
     run = True
     while run:
-        
-        
+        WIN.blit(bg_IMGS[0],(0,0))
+
         p1.move()
         #pygame.draw.rect(WIN,(255,0,0),(10,10,50,50))
         clock.tick(30)
