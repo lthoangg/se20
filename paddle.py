79dd555b7e7e62c53f76cb92bebf2793e7843ec3
dtkienn <dtkien12300@gmail.com>
2020-03-04 15:43:40 +0700

    add paddle

--- paddle.py
+++ paddle.py
@@ -1,25 +1,19 @@
 import pygame
 import os
 import time
-import pong
 
-x = 50
-y = 50
-width = 40
-height = 60
-vel = 5
-run = True
-while run:
-    keys= pygame.key.get_pressed()
-    # if keys[pygame.K_LEFT]:
-
-    # if keys[pygame.K_RIGHT]:
-
-    # if keys[pygame.K_UP]:
-
-    if keys[pygame.K_DOWN]:
-        y+=vel
-    pygame.draw.rect(WIN, (255,0,0), (x, y, width, height))
-    pygame.display.update()
+class Paddle:
+    x = 500
+    y = 300
+    width = 40
+    height = 150
+    vel = 0
+    def move(self):
+        keys= pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            y+=20
+        if keys[pygame.K_DOWN]:
+            y-=20
+        pygame.display.update()
 
     
