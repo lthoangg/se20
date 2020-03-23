94e3b0cb722176d3debea8a39423001130722fa9
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-03-23 11:04:13 +0700

    Fixed pixels

--- ball.py
+++ ball.py
@@ -5,21 +5,21 @@ import os
 import time
 BALL_IMGS = [pygame.image.load(os.path.join("imgs", "ball1.png"))]
 class Ball:
-    r=10
-    vel = 25
-    def __init__(self):
-        self.r = 10
-        self.x=512-self.r
-        self.y=275-self.r
+    r=20
+    vel = 12
+    def __init__(self, WIN):
+        self.WIN = WIN
+        self.x= WIN.get_width()/2 - self.r
+        self.y= WIN.get_height()/2 - self.r
         
-        self.img = pygame.transform.scale(BALL_IMGS[0],(40,40))
+        self.img = pygame.transform.scale(BALL_IMGS[0],(2*self.r, 2*self.r))
 
         ran=random.randint(1,2)
         if ran==1: self.vel = -self.vel
         else: self.vel = self.vel
     def move(self):
         self.x+=self.vel
-        if self.x <=0 or self.x >=1024:
+        if self.x <=0 or self.x >= self.WIN.get_width() - self.r*2:
             self.vel = -self.vel
     
     def draw(self,WIN):
