492440c4771742be6c655b9a873ba122435cde3b
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-03-31 20:43:42 +0700

    Fixed menu and main

--- main.py
+++ main.py
@@ -4,22 +4,16 @@ import time
 import paddle
 import ball
 import background
+import Color
 
+pygame.init()
 display_width = 1280
 display_height = 720
 
-black = (0, 0, 0)
-white = (255, 255, 255)
-red = (255, 0, 0)
-green = (0, 200, 0)
-bright_red = (255, 0, 0)
-bright_green = (0, 255, 0)
-
-block_color = (53, 115, 255)
+#block_color = (53, 115, 255)
 
 windows = pygame.display.set_mode((display_width, display_height))
 pygame.display.set_caption('Ping Pong')
-clock = pygame.time.Clock()
 
 
 # Drawing graphic
@@ -35,17 +29,18 @@ def move(p1, p2, b):
     p1.move()
     p2.move()
     b.move()
+    b.collide(p1)
+    b.collide(p2)
 
 
 def text_objects(text, font):
-    textSurface = font.render(text, True, black)
+    textSurface = font.render(text, True, Color.black)
     return textSurface, textSurface.get_rect()
 
 
 def button(msg, x, y, w, h, ic, ac, action=None):
     mouse = pygame.mouse.get_pos()
     click = pygame.mouse.get_pressed()
-    print(click)
     if x + w > mouse[0] > x and y + h > mouse[1] > y:
         pygame.draw.rect(windows, ac, (x, y, w, h))
 
@@ -56,34 +51,33 @@ def button(msg, x, y, w, h, ic, ac, action=None):
 
     smallText = pygame.font.SysFont("comicsansms", 20)
     textSurf, textRect = text_objects(msg, smallText)
-    textRect.center = ((x + (w / 2)), y + (h / 2))
+    textRect.center = ((x + int(w / 2)), y + int(h / 2))
     windows.blit(textSurf, textRect)
 
 #Main menu
-def game_intro():
-    #background_image = pygame.image.load("imgs", "menu.png")
+def menu():
+    background_image = pygame.transform.scale2x(pygame.image.load(os.path.join("imgs", "menu.png")))
 
     pygame.init()
     intro = True
 
     while intro:
         for event in pygame.event.get():
-            print(event)
             if event.type == pygame.QUIT:
                 pygame.quit()
                 quit()
 
-        windows.fill(white)
+        windows.fill(Color.white)
+        windows.blit(background_image,(300, 50))
         largeText = pygame.font.Font('freesansbold.ttf', 115)
         TextSurf, TextRect = text_objects("Ping Pong", largeText)
-        TextRect.center = ((display_width / 2), (display_height / 2))
+        TextRect.center = (int(display_width / 2), int(display_height / 2))
         windows.blit(TextSurf, TextRect)
 
-        button("GO!", 450, 450, 100, 50, green, bright_green, main)
-        button("Quit", 750, 450, 100, 50, red, bright_red, exit)
+        button("GO!", 450, 450, 100, 50, Color.green, Color.bright_green, main)
+        button("Quit", 750, 450, 100, 50, Color.red, Color.bright_red, exit)
 
         pygame.display.update()
-        clock.tick(15)
 
 
 # Main game
@@ -107,19 +101,8 @@ def main():
 
     # Game running
     while run:
-        # pygame.draw.rect(WIN,(255,0,0),(10,10,50,50))
-        clock.tick(30)  # FPS of game
+        clock.tick(120)  # FPS of game
         for event in pygame.event.get():
-            # Mouse click
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                if event.button == 1:
-                    print('left')
-                elif event.button == 2:
-                    print('middle')
-                elif event.button == 3:
-                    print('right')
-                print(str(pygame.mouse.get_pos()[0]) + " " + str(pygame.mouse.get_pos()[1]))
-
             # Quit button            
             if event.type == pygame.QUIT:
                 print("Out game")
@@ -127,15 +110,10 @@ def main():
                 pygame.quit()
                 quit()
 
-        if b.collide(p1) is True or b.collide(p2) is True:
-            pass
-
         draw(WIN, bg, p1, p2, b)
         move(p1, p2, b)
         pygame.display.update()
 
 
-game_intro()
-main()
-pygame.quit()
-quit()
+if __name__ == "__main__":
+    menu()
