2b280f9f64ae5596088213b0751ce4680e745b27
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-03-31 20:43:22 +0700

    Fixed collide and wall

--- ball.py
+++ ball.py
@@ -1,45 +1,65 @@
 import pygame
 import random
-import paddle
 import os
 import time
 BALL_IMGS = [pygame.image.load(os.path.join("imgs", "ball1.png"))]
 class Ball:
     r=20
-    vel = 12
+    vx = 5
     def __init__(self, WIN):
         self.WIN = WIN
         self.x= WIN.get_width()/2 - self.r
         self.y= WIN.get_height()/2 - self.r
-        
+        self.vy = random.randint(-4,5)
         self.img = pygame.transform.scale(BALL_IMGS[0],(2*self.r, 2*self.r))
-
         ran=random.randint(1,2)
-        if ran==1: self.vel = -self.vel
-        else: self.vel = self.vel
+        if ran==1: self.vx = - self.vx
+        
+        ran = random.randint(1, 9)
+        print(ran)
+        if ran <= 4:
+            self.vy = ran 
+        else: self.vy = ran - 9
+    
+
 
     def move(self):
-        self.x+=self.vel
+        self.x += self.vx
+        self.y += self.vy
         if self.x <=0 or self.x >= self.WIN.get_width() - self.r*2:
-            self.vel = -self.vel
-        #if self.collide(paddle):
-        #    self.vel = -self.vel
+            print("Oops")
+        
+        if self.y <= 0:
+            self.vy = abs(self.vy)
+            print("wall top")
+        if self.y + 2*self.r == 720:
+            self.vy = - abs(self.vy)
+            print("Wall bottom")
     
     def draw(self,WIN):        
         WIN.blit(self.img,(self.x,self.y))
 
     def collide(self, paddle):
-        paddle_mask = paddle.get_mask()
-        mask = pygame.mask.from_surface(self.img)
-        offset = ( round(self.x) - paddle.x, round(self.y) - paddle.y)
-
-        point = paddle_mask.overlap(mask, offset)
-        print(point)
-        if point is not None:
-            print('collide')
-            return True
-        else:
-            return False
-
-
         
+        # Left: 35
+        # Right: 1205
+        if paddle.y+100 > self.y >= paddle.y:
+            if paddle.player == 1 and self.x == 35:
+                print("left")
+                self.vx = -self.vx
+                
+                ran = random.randint(1, 9)
+                print(ran)
+                if ran <= 4:
+                    self.vy = ran 
+                else: self.vy = ran - 9
+            if paddle.player == 2 and self.x == 1205:
+                print("Right")
+                self.vx = -self.vx
+
+                ran = random.randint(1, 9)
+                print(ran)
+                if ran <= 4:
+                    self.vy = ran 
+                else: self.vy = ran - 9
+            
