9a770b6d61ccdf6da9d496efbc35015918097a1f
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-04-24 21:11:48 +0700

    [fix] players, [bug] cannot call class

--- paddle.py
+++ paddle.py
@@ -1,14 +1,20 @@
 import pygame
 import os
 import time
-
-PADDLE_IMGS = [pygame.image.load(os.path.join("imgs/paddles","paddle1.png"))]
+PADDLE_IMGS = [pygame.image.load(os.path.join("imgs/paddles","paddle1.png")), pygame.image.load(os.path.join("imgs/paddles","paddle2.png")), pygame.image.load(os.path.join("imgs/paddles","paddle3.png")), pygame.image.load(os.path.join("imgs/paddles","paddle4.png")), pygame.image.load(os.path.join("imgs/paddles","paddle5.png")), pygame.image.load(os.path.join("imgs/paddles","paddle6.png")), pygame.image.load(os.path.join("imgs/paddles","paddle7.png")), pygame.image.load(os.path.join("imgs/paddles","paddle8.png"))]
 
 class Paddle(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, player=1):
         pygame.sprite.Sprite.__init__(self)
-        self.image = PADDLE_IMGS[0]
+        self.image = self.PADDLE_IMGS[0]
         self.rect = self.image.get_rect()
+        self.player = player
+        if self.player==1:
+            self.x = 20
+        elif self.player ==2:
+            self.x = 685
+        self.y = 310
+        self.vel =0
 
     def move(self):
         self.y+=self.vel
