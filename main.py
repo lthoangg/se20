7022b35c1de703ada5098b6ff5e488737382bc7b
lthoangg <hoanglt.bi9241@st.usth.edu.vn>
2020-04-24 20:48:33 +0700

    [test] add paddles

--- main.py
+++ main.py
@@ -1,41 +1,85 @@
 import sys
+import os
 # import time
 import pygame
 import Color
 import Text
 import Frame
 import Button
+import ball
+import paddle
+import background
 # Initial game
+pygame.init()
+menu_image = pygame.transform.scale(pygame.image.load(os.path.join("imgs","menu.png")), (680, 720))
+paddle = pygame.image.load(os.path.join("imgs/paddles", "paddle1.png"))
 def menu():
     window_width = 1280
     window_height = 720
     window_game = pygame.display.set_mode((window_width, window_height))
     pygame.display.set_caption("Ping Pong")
 
-    name = Text.text("PING PONG GAME")
+    name = Text.text("PING PONG GAME", 32)
     heading = Frame.frame()
-    play = Button.button("PLAY", 2)
+    play = Button.button("PLAY")
+    score_board = Button.button("Scoreboard",)
+    how_to_play = Button.button("How to play")
+    about_us = Button.button("About us")
+    step = 5
 
-    clock = pygame.time.Clock()
+    window_game.fill(Color.gray)
+    window_game.blit(menu_image, (340,0 ))
+    heading.blit_center_top(window_game)
+    name.blit_center_frame(window_game, heading)
+
+    play.draw(window_game)
+
+    score_board.draw(window_game, (play.frame.x, play.frame.y+play.frame.height+ step))
+    how_to_play.draw(window_game, (score_board.frame.x, score_board.frame.y +score_board.frame.height + step))
+    about_us.draw(window_game, (how_to_play.frame.x, how_to_play.frame.y + how_to_play.frame.height + step))
+
     run = True
     # Running
     while run:
-        clock.tick(60)
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                print()
                 run = False
                 sys.exit()
             
             if event.type == pygame.MOUSEBUTTONDOWN:
-                print(event.pos, play.is_Click(event.pos))
-
-        window_game.fill(Color.gray)
-        heading.blit_center_top(window_game)
-        name.blit_center_frame(window_game, heading)
-        play.draw(window_game)
+                if play.is_Click(event.pos):
+                    Play(window_game)
+                    print("click play")
+                elif score_board.is_Click(event.pos):
+                    print("click scoreboard")
+                elif how_to_play.is_Click(event.pos):
+                    print("click how_to play")
+                elif about_us.is_Click(event.pos):
+                   print("click about us")
+                else:
+                    print("Missclick")
         
         pygame.display.update()
+
+def Play(WIN):
+    WIN.fill(Color.white)
+
+    back_ground = background.Background()
+    b = ball.Ball(WIN)
+    p1 = paddle.Paddle()
+
+    run =True
+    # Running
+    while run:
+        for event in pygame.event.get():
+            pass
+
+        
+        back_ground.draw(WIN)
+        b.draw(WIN)
+        p1.draw(WIN)
+        pygame.display.update()
+
 
 if __name__ == "__main__":
     menu()
